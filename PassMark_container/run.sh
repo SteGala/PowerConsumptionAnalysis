520f8ea85dbcedbd63e668b6a26c4fe1b064e6c4
SteGala <galaxx.ste@gmail.com>
2021-12-01 08:34:22 +0100

    Updated run scripts

--- PassMark_container/run.sh
+++ PassMark_container/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-./pt_linux_x64 -r 1 -d 3 -i 5
+./pt_linux_x64 -r 1 -d $TESTLEN -i $TESTITERATIONS
+#./pt_linux_x64 -r 1 -d 3 -i 5
 mv results_cpu.yml "./data/results_cpu_$CURRENTLOAD.yml"
