1893bbec56d620f3929cfe1b5233f63f5cb59222
SteGala <galaxx.ste@gmail.com>
2021-12-02 12:52:26 +0100

    add xiaomi laptop data

--- scripts/scrape_process_metrics.sh
+++ scripts/scrape_process_metrics.sh
@@ -16,29 +16,29 @@ do
     echo "#### $(date +%s)" >> ./data/memory_usage
     echo "#### $(date +%s)" >> ./data/cpu_usage
 
-    #cmd=$(top -b -n2 -d 0.1)  
-    #ntask=$(echo "$cmd" | grep "Tasks:" | tail -n 1 | cut -d " " -f 2)
-    #taskList=$(echo "$cmd" | tail -n $ntask | gawk '{print $1,$9,$10,$12}')
+    cmd=$(top -b -n2 -d 0.1)  
+    ntask=$(echo "$cmd" | grep "Tasks:" | tail -n 1 | cut -d " " -f 2)
+    taskList=$(echo "$cmd" | tail -n $ntask | gawk '{print $1,$9,$10,$12}')
     
     cmdDocker=$(docker stats --no-stream | grep "passmark_container" | gawk '{print $3}' | cut -d "%" -f 1)
     echo "$(date +%s) $cmdDocker" >> ./data/cpu_usage_docker
 
-    #while read z
-    #do
-        #process=$(echo $z | cut -d " " -f 4)
-        #pid=$(echo $z | cut -d " " -f 1)
-        #pcpu=$(echo $z | cut -d " " -f 2)
-        #pmem=$(echo $z | cut -d " " -f 3)
+    while read z
+    do
+        process=$(echo $z | cut -d " " -f 4)
+        pid=$(echo $z | cut -d " " -f 1)
+        pcpu=$(echo $z | cut -d " " -f 2)
+        pmem=$(echo $z | cut -d " " -f 3)
 
-        #echo "$pid $process $pcpu" >> ./data/cpu_usage
-        #echo "$pid $process $pmem" >> ./data/memory_usage
+        echo "$pid $process $pcpu" >> ./data/cpu_usage
+        echo "$pid $process $pmem" >> ./data/memory_usage
 
         #totcpu=$(echo "scale=3;$totcpu+$pcpu" | bc)
         #totram=$(echo "scale=3;$totram+$pmem" | bc)
 
         #cpu="${cpu}scraper_cpu_usage_percentage{process=\"${process}\", pid=\"${pid}\", hostname=\"${hostname}\"} ${pcpu}"$'\n'
         #mem="${mem}scraper_memory_usage_percentage{process=\"${process}\", pid=\"${pid}\", hostname=\"${hostname}\"} ${pmem}"$'\n'
-    #done <<< "$taskList"
+    done <<< "$taskList"
     
     #echo "$(date +%s) $(echo "scale=3;$totcpu/$thread" | bc)" >> ../data/cpu_usage
     #echo "$(date +%s) $totcpu" >> ../data/cpu_usage
